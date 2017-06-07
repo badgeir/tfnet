a558e8c88f132b41d7649c93a9d7163176092575
Peter <pleupi123@gmail.com>
2017-06-07 19:18:17 +0200

    setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import sys
+import os.path
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'tfnet'))
+
+setup(name='tfnet',
+      version=0.1,
+      description='A helper library for tensorflow.',
+      url='https://github.com/badgeir/tfnet',
+      author='Peter Leupi',
+      author_email='pleupi123@gmail.com',
+      license='MIT',
+      packages=[package for package in find_packages()
+                if package.startswith('tfnet')],
+)
